b616e47e5a5228d535481f2ebc55650bb2388c3d
Zachary Upton <114755625+GlamrockGlitchtrap@users.noreply.github.com>
2023-02-23 10:12:26 -0600

    Initial commit

---
+++ hello world/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks user for first name
+            Console.WriteLine("what is your first name?");
+            // Assigns user input to firstName variable
+            string firstName = Console.ReadLine();
+            // Asks the user for last name
+            Console.WriteLine("What is your last name");
+            // Assigns user iput to lastName variable
+            string lastName = Console.ReadLine();          
+
+            Console.WriteLine($"Hellow, {firstName} {lastName}!");
+        }
+    }
+}
